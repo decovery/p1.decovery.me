ec62a3b4d7ac05562b6bb305cc8ddbc05db53b94
decovery <aiste@akmila.lt>
2013-09-16 13:43:36 +0300

    index file changed, styles and images added

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-Welcome to P1 !!!!
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8"/>
+<link rel="stylesheet" href="style.css" />
+<link  rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" />
+<title>Project 1</title>
+</head>
+
+<body>
+<section>
+<img src="img/photo.jpg" title="Aiste" alt="Aiste Ciurlyte Kolesnicenko" >
+<h1>Aiste Ciurlyte Kolesnicenko</h1>
+<p>My name is Aiste (pronounced somewhat as Ice tea). I’m taking the course completely online as I live in Europe, small country called Lithuania.</p>
+<p>I work as web designer for a few recent years and I’m passionate about CSS and Responsive web design.</p>
+<p>I use a code editor called Espresso by MacRabbit.</p>
+</section>
+</body>
+</html>
