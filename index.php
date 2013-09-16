8bd7afc163b476300375d714cd37a21e38f24c80
decovery <aiste@akmila.lt>
2013-09-16 16:06:45 +0300

    Some style and media queries added

--- index.php
+++ index.php
@@ -9,14 +9,22 @@
 </head>
 
 <body>
-<section>
-<img src="img/photo.jpg" title="Aiste" alt="Aiste Ciurlyte Kolesnicenko" >
-<h2>Bio</h2>
-<p>My name is Aiste (pronounced somewhat as Ice tea). I’m taking the course completely online as I live in Europe, small country called Lithuania.</p>
-<p>This is my first course at HES towards Web technologies certificate.</p>
-<p>I work as web designer for a few recent years and I’m passionate about CSS and Responsive web design.</p>
-<h2>Code editor</h2>
-<p>I use a code editor called Espresso by MacRabbit.</p>
-</section>
+	<div id="main-content">
+		<div id="main-image">
+			<img src="img/photo.jpg" title="Aiste" alt="Aiste Ciurlyte Kolesnicenko" >
+		</div>
+		<section>
+			<h2>Bio</h2>
+			<p>My name is Aiste <i>(pronounced somewhat as Ice tea)</i>. I’m taking the course completely online as I live in Europe, small country called Lithuania.</p>
+			<p>This is my first course at HES towards Web technologies certificate.</p>
+			<p>I work as web designer for a few recent years and I’m passionate about CSS and Responsive web design.</p>
+			<hr />
+		</section>
+		<section>
+			<h2>Code editor</h2>
+			<p>I use a code editor called Espresso by MacRabbit.</p>
+			<hr />
+		</section>
+	</div>
 </body>
 </html>
