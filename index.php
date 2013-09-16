a9511eb62c8082947e8d9d4c1da0c34d4e1becd4
decovery <aiste@akmila.lt>
2013-09-16 14:19:42 +0300

    index file change

--- index.php
+++ index.php
@@ -11,9 +11,11 @@
 <body>
 <section>
 <img src="img/photo.jpg" title="Aiste" alt="Aiste Ciurlyte Kolesnicenko" >
-<h1>Aiste Ciurlyte Kolesnicenko</h1>
+<h2>Bio</h2>
 <p>My name is Aiste (pronounced somewhat as Ice tea). I’m taking the course completely online as I live in Europe, small country called Lithuania.</p>
+<p>This is my first course at HES towards Web technologies certificate.</p>
 <p>I work as web designer for a few recent years and I’m passionate about CSS and Responsive web design.</p>
+<h2>Code editor</h2>
 <p>I use a code editor called Espresso by MacRabbit.</p>
 </section>
 </body>
